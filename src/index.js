c57dfae9206419dac1c2c8a5163b5f032494567d
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-03-21 18:50:51 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+      minValue = 0;
+  } else {
+      minValue = array[0];
+      for (let j = 0; j < array.length; j++) {
+          if (minValue > array[j]) {
+              minValue = array[j];
+          }
+      }
+  }
+  return minValue;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) {
+    return 0;
+} else {
+    let maxValue = array[0];
+    for (let j = 0; j < array.length; j++) {
+        if (maxValue < array[j]) {
+          maxValue = array[j];
+        }
+    }
+    return maxValue;
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+  if (array === undefined || array.length === 0) {
+      return 0;
+  } else {
+      for(let i = 0; i < array.length; i++) {
+          sum += array[i];
+      }
+      return avgValue = sum/array.length;
+  }
+};
